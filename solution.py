b3ad0da533683730a3ba88d89282c2683fb25dba
Rene Dohmen <acidjunk@gmail.com>
2018-11-07 19:57:10 +0100

    More stuff

--- solution.py
+++ solution.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 
 import csv
 import structlog
+import helper
 
 from ortools.constraint_solver import pywrapcp
 from ortools.constraint_solver import routing_enums_pb2
@@ -18,14 +19,25 @@ logger.info("Started")
 def create_data_model():
     """Stores the data for the problem"""
 
+    _limit = 100
+    # _limit = 100000
+
     _locations = [(90, 0), ]
     demands = [0, ]
 
     with open('gifts.csv') as csvfile:
         reader = csv.DictReader(csvfile)
+        counter = 0
         for row in reader:
-            _locations.append((float(row["Latitude"]),float(row["Longitude"])))
-            demands.append(float(row["Weight"]))
+            if counter <= _limit:
+                counter += 1
+                _locations.append((float(row["Latitude"]), float(row["Longitude"])))
+                demands.append(float(row["Weight"]))
+
+    logger.debug("locations", locations=_locations)
+    logger.debug("weights", weights=demands)
+    logger.info("locations", locations=len(_locations))
+    logger.info("weights", weights=len(demands))
 
     data = {}
 
@@ -62,8 +74,10 @@ def create_distance_callback(data):
                 _distances[from_node][to_node] = 0
             else:
                 _distances[from_node][to_node] = (
-                    manhattan_distance(data["locations"][from_node],
-                                       data["locations"][to_node]))
+                    helper.haversine(data["locations"][from_node][0],
+                                     data["locations"][from_node][1],
+                                     data["locations"][to_node][0],
+                                     data["locations"][to_node][1]))
 
     def distance_callback(from_node, to_node):
         """Returns the manhattan distance between the two nodes"""
@@ -106,9 +120,12 @@ def print_solution(data, routing, assignment):
         while not routing.IsEnd(index):
             node_index = routing.IndexToNode(index)
             next_node_index = routing.IndexToNode(assignment.Value(routing.NextVar(index)))
-            route_dist += manhattan_distance(
-                data["locations"][node_index],
-                data["locations"][next_node_index])
+            route_dist += helper.haversine(
+                data["locations"][node_index][0],
+                data["locations"][node_index][1],
+                data["locations"][next_node_index][0],
+                data["locations"][next_node_index][1]
+            )
             route_load += data["demands"][node_index]
             plan_output += ' {0} Load({1}) -> '.format(node_index, route_load)
             index = assignment.Value(routing.NextVar(index))
@@ -129,6 +146,7 @@ def main():
     """Entry point of the program"""
     # Instantiate the data problem.
     data = create_data_model()
+    logger.info("Data model loaded")
     # Create Routing Model
     routing = pywrapcp.RoutingModel(
         data["num_locations"],
